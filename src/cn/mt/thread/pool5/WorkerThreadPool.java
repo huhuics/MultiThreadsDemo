aad0994392aa2fafd6efcdb0cfa08b1a7340fe23
huhuics <hhuics@163.com>
2017-02-09 23:55:39 +0800

    增加WorkerThreadPool

---
+++ src/cn/mt/thread/pool5/WorkerThreadPool.java
@@ -0,0 +1,21 @@
+/**
+ * 深圳金融电子结算中心
+ * Copyright (c) 1995-2017 All Rights Reserved.
+ */
+package cn.mt.thread.pool5;
+
+/**
+ * 
+ * @author HuHui
+ * @version $Id: WorkerThreadPool.java, v 0.1 2017年2月9日 下午11:55:17 HuHui Exp $
+ */
+public class WorkerThreadPool {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
