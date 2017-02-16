5b2516bd42c163dc163c6cf1ada5a122acb5e064
huhuics <hhuics@163.com>
2017-02-16 21:09:49 +0800

    增加semaphore测试

---
+++ src/cn/mt/semaphore/App.java
@@ -0,0 +1,54 @@
+/**
+ * 深圳金融电子结算中心
+ * Copyright (c) 1995-2017 All Rights Reserved.
+ */
+package cn.mt.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Semaphore测试类
+ * @author HuHui
+ * @version $Id: App.java, v 0.1 2017年2月16日 下午8:04:07 HuHui Exp $
+ */
+public class App {
+
+    /**
+     * @param args
+     * @throws InterruptedException 
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(20);
+
+        final ConnectionFactory connectionFactory = ConnectionFactory.getInstance();
+
+        for (int i = 0; i < 20; i++) {
+            threadPool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    connectionFactory.doConnecte();
+                }
+            });
+        }
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.DAYS);
+
+        /* Semaphore semaphore = new Semaphore(1);
+         System.out.println("0.permits: " + semaphore.availablePermits());
+         semaphore.acquire();
+         System.out.println("1.permits: " + semaphore.availablePermits());
+         semaphore.release(2);
+         System.out.println("2.permits: " + semaphore.availablePermits());
+         semaphore.release();
+         System.out.println("3.permits: " + semaphore.availablePermits());
+         semaphore.acquire();
+         System.out.println("4.permits: " + semaphore.availablePermits());
+         semaphore.release(4);
+         System.out.println("5.permits: " + semaphore.availablePermits());*/
+    }
+
+}
---
+++ src/cn/mt/semaphore/ConnectionFactory.java
@@ -0,0 +1,50 @@
+/**
+ * 深圳金融电子结算中心
+ * Copyright (c) 1995-2017 All Rights Reserved.
+ */
+package cn.mt.semaphore;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 生成连接的工程
+ * @author HuHui
+ * @version $Id: ConnectionFactory.java, v 0.1 2017年2月16日 下午7:56:56 HuHui Exp $
+ */
+public class ConnectionFactory {
+
+    private static ConnectionFactory instance  = new ConnectionFactory();
+
+    private Semaphore                semaphore = new Semaphore(10);
+
+    private Random                   random    = new Random(3000L);
+
+    private ConnectionFactory() {
+
+    }
+
+    public static ConnectionFactory getInstance() {
+        return instance;
+    }
+
+    public void doConnecte() {
+        try {
+            semaphore.acquire();
+            System.out.println(Thread.currentThread().getName() + " 获取到了一个许可,许可还剩" + semaphore.availablePermits());
+
+            if (semaphore.availablePermits() == 0) {
+                System.out.println("许可用完,扩大许可");
+                semaphore.release(15);
+                System.out.println("扩大后许可数量" + semaphore.availablePermits());
+            }
+
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+            System.out.println(Thread.currentThread().getName() + " 释放了一个许可,许可还剩" + semaphore.availablePermits());
+        }
+    }
+}
